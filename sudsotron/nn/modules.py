048813eac0a004986c2df22bfe2f236f1ddcfb2b
Dominic Rufa <46943710+dominicrufa@users.noreply.github.com>
2024-04-03 19:01:30 -0400

    add utils for nn modules and init

---
+++ sudsotron/nn/modules.py
@@ -0,0 +1,105 @@
+"""Write some modules in flax.linen"""
+import jax 
+from jax import numpy as jnp
+import typing
+
+from flax import linen as nn
+
+from sudsotron.nn import utils as nnutls
+
+
+class MLP(nn.Module):
+    """simple multilayer perceptron"""
+    features: int # hidden layer features
+    num_layers: int # number of layers
+    output_dimension: int # output dimension
+    nonlinearity: typing.Callable[float, float] = jnp.tanh
+    activate_output: bool=False # whether to activate output
+    
+    @nn.compact
+    def __call__(self, x: jax.Array, **unused_kwargs) -> jax.Array:
+        # NOTE: may want to extract `Dense` unused_kwargs out in future.
+        for layer in range(self.num_layers):
+            feats = self.features if layer < self.num_layers-1 else self.output_dimension
+            x = nn.Dense(features = feats)(x)
+            if layer != self.num_layers - 1 and not self.activate_output:
+                x = self.nonlinearity(x)
+        return x
+    
+class GaussianBasis(nn.Module):
+    """compute a gaussian basis projection of `x`"""
+    bounds: jax.Array # [N,2], N is dim of `x`
+    projection_dim: int # number of projections for each entry of `x`
+
+    @nn.compact
+    def __call__(self, x: jax.Array, **unused_kwargs) -> jax.Array:
+        out = nnutls.gaussian_basis_projections(x, self.bounds, self.projection_dim).flatten()
+        return out
+
+class GaussianBasisMLP(nn.Module):
+    """project `x` to a gaussian basis and then pass through an MLP"""
+    bounds: jax.Array 
+    projection_dim: int
+    features: int
+    num_layers: int
+    output_dimension: int
+    nonlinearity: typing.Callable[float, float] = jnp.tanh
+    activate_output: bool=False
+    
+    @nn.compact
+    def __call__(self, x, **unused_kwargs) -> jax.Array:
+        x = GaussianBasis(bounds=self.bounds, projection_dim=self.projection_dim)(x)
+        x = MLP(self.features, self.num_layers, self.output_dimension, self.nonlinearity, self.activate_output)(x)
+        return x
+
+class BasePINNHidden(nn.Module):
+    """hidden layer of PINN"""
+    U: jax.Array # [N_U,]
+    V: jax.Array # [N_V,]
+    features: int # MLP hidden features
+    num_layers: int # num MLP layers
+    output_dimension: int
+    nonlinearity: typing.Callable[float, float] = jnp.tanh
+    
+    @nn.compact
+    def __call__(self, H: jax.Array, **unused_kwargs):
+        Z = MLP(self.features, self.num_layers, self.output_dimension, self.nonlinearity, True)(H)
+        H = (1. - Z) * self.U + Z * self.V
+        return H
+
+class BasePINN(nn.Module):
+    """Eqs. 43-47 (Fig. 9) of https://arxiv.org/pdf/2001.04536.pdf'"""
+    features: int # num features of initial MLP
+    num_layers: int # num layers of initial MLP
+    output_dimension: int # final output dim
+    num_outer_layers: int=1 # number of PINN layers
+    nonlinearity: typing.Callable[float, float] = jnp.tanh
+    # activate_output: bool=True; this is always True because a nonactivated output is at end
+    
+    @nn.compact
+    def __call__(self, x: jax.Array, **unused_kwargs) -> jax.Array:
+        UVH = MLP(self.features, self.num_layers, 3*self.features, self.nonlinearity, True)(x)
+        U, V, H = UVH[:self.features], UVH[self.features:2*self.features], UVH[2*self.features:]
+        for k in range(self.num_outer_layers):
+            H = BasePINNHidden(U, V, self.features, self.num_layers, self.features, self.nonlinearity)(H)
+        return nn.Dense(features=self.output_dimension)(H)
+
+class GaussianBasisPINN(nn.Module):
+    """a `GaussianBasis` projection followed by a full `BasePINN`"""
+    # Gaussian Basis kwargs
+    bounds: jax.Array
+    projection_dim: int
+    
+    # BasePINN kwargs
+    features: int
+    num_layers: int
+    output_dimension: int
+    num_outer_layers: int
+    nonlinearity: typing.Callable[float, float] = jnp.tanh
+    
+    @nn.compact
+    def __call__(self, x: jax.Array, **unused_kwargs) -> jax.Array:
+        x = GaussianBasis(self.bounds, self.projection_dim)(x)
+        x = BasePINN(self.features, self.num_layers, 
+                     self.output_dimension, self.nonlinearity, self.num_outer_layers)(x)
+        return x
---
+++ sudsotron/nn/utils.py
@@ -0,0 +1,68 @@
+"""utilities for nn modules"""
+import jax
+from jax import numpy as jnp
+
+def unnormalized_gaussian(x: float, mu: float, sigma: float, **unused_kwargs) -> float:
+    return jnp.exp(-0.5 * ((x - mu) / sigma)**2)
+
+def gaussian_basis(x: float, mus: jax.Array, sigma: float, **unused_kwargs) -> float:
+    """return a gaussian projection of `x` given an array of mus, one sigma"""
+    return jax.vmap(unnormalized_gaussian, in_axes=(None, 0, None))(x, mus, sigma)
+
+def gaussian_basis_projections(
+    x: jax.Array, # [N,]
+    bounds: jax.Array, # [N,2]
+    projection_dim: int, 
+    **unused_kwargs) -> jax.Array:
+    """for each entry of `x`, `bounds`, project x into a gaussian basis of 
+    dimension `projection_dim` with sigma given by (bound[1] - bound[0])/projection_dim"""
+    mus, sigmas = jax.vmap(jnp.linspace, 
+                           in_axes=(0,0,None, None, None)
+                           )(bounds[:,0], bounds[:,1], projection_dim, True, True)
+    return jax.vmap(gaussian_basis)(x, mus, sigmas)
+
+def log_prob_gaussian_mixture(x: float, 
+                              mus: jax.Array, 
+                              sigmas: jax.Array, 
+                              log_weights: jax.Array) -> float:
+    """
+    Computes the log probability of a 1D random variable x drawn from a gaussian
+    mixture model.
+
+    Args:
+    x: A jnp.array of shape (N,) containing the data points.
+    mus: A jnp.array of shape (K,) containing the means of the components.
+    sigmas: A jnp.array of shape (K,) containing the standard deviations of the
+      components.
+    log_weights: A jnp.array of shape (K,) containing the log mixing coefficients.
+
+    Returns:
+    A float of the log probability
+    """
+    # Normalize log weights
+    norm_log_weights = log_weights - jax.scipy.special.logsumexp(log_weights)
+
+    # Compute the log probabilities of x under each component.
+    unweighted_log_probs = jax.scipy.stats.norm.logpdf(x, loc=mus, scale=sigmas)
+    log_probs = norm_log_weights + unweighted_log_probs
+
+    # Compute the log probability of x under the mixture model.
+    log_prob = jax.scipy.special.logsumexp(log_probs)
+
+    return log_prob
+
+def prob_gaussian_mixture(x: float, mus: jax.Array, 
+                              sigmas: jax.Array, 
+                              log_weights: jax.Array) -> float:
+    """compute `log_prob_gaussian_mixture` and exp it to get normalized prob"""
+    log_prob = log_prob_gaussian_mixture(x, mus, sigmas, log_weights)
+    return jnp.exp(log_prob)
+
+def cosine_cutoff(r: float, r_cut: float) -> float:
+    """cosine cutoff function that decays smoothly to zero at `r_cut`"""
+    return jax.lax.select(r >= r_cut, 0 * r, 1 + jnp.cos(r * jnp.pi / r_cut)) * 0.5
+
+def r_midpoints(bin_edges: jax.Array, # [N,] 
+                **unused_kwargs) -> jax.Array:
+    """compute the midpoint of an array of bin edges"""
+    return (bin_edges[:-1] + bin_edges[1:])/2.
