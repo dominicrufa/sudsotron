73d970dd033bda0d6cfa6c6ff9a9ce752c406d3f
Dominic Rufa <46943710+dominicrufa@users.noreply.github.com>
2024-04-16 14:22:31 -0400

    remove constants import

--- sudsotron/cDFT/__init__.py
+++ sudsotron/cDFT/__init__.py
@@ -26,12 +26,6 @@ from sudsotron.potentials import (
     PotentialHandler,
 )
 
-from sudsotron.cDFT.constants import (
-    DEFAULT_KT, 
-    DEFAULT_GRID_FLOATTYPE,
-    DEFAULT_N0
-)
-
 DEFAULT_KT = 2.48 # amu * nm^2 / (ps^2 * particle) # this is 1kT at ~300K
 DEFAULT_N0 = 32.776955 # particles/nm**3 (taken from dcf data for TIP3P at ambient conditions w/ RF electrostatics)
 DEFAULT_M = 18. # amus (mass of H2O mol)
