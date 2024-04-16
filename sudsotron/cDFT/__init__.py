6fd4b75badddc588f0fb5e73870ffd4a6af000a2
Dominic Rufa <46943710+dominicrufa@users.noreply.github.com>
2024-04-16 14:13:51 -0400

    remove constants

--- sudsotron/cDFT/__init__.py
+++ sudsotron/cDFT/__init__.py
@@ -32,6 +32,12 @@ from sudsotron.cDFT.constants import (
     DEFAULT_N0
 )
 
+DEFAULT_KT = 2.48 # amu * nm^2 / (ps^2 * particle) # this is 1kT at ~300K
+DEFAULT_N0 = 32.776955 # particles/nm**3 (taken from dcf data for TIP3P at ambient conditions w/ RF electrostatics)
+DEFAULT_M = 18. # amus (mass of H2O mol)
+DEFAULT_R_CUT = 1.2
+DEFAULT_GRID_FLOATTYPE = jnp.float32
+
 def dcf_helper(
         r: float, 
         params: NNParams, 
