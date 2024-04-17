2c74f2b1f05808e8f920dec64907b7e0665dd2a8
Dominic Rufa <46943710+dominicrufa@users.noreply.github.com>
2024-04-17 18:02:17 -0400

    mute kkfe for bad imports

--- sudsotron/cDDFT/KKFE.py
+++ sudsotron/cDDFT/KKFE.py
@@ -47,81 +47,81 @@ DEFAULT_NUM_MEAN_VELOCITY_PROJECTIONS = 32
 
 
 
-@dataclass(frozen=True)
-class SSKKFE: # spherically symmetric KKFE
-    hnc_dcf: HNCRadialDCF
-    Uext_handler: typing.Union[
-        DynamicPotentialHandler,
-        DynamicNeuralPotentialHandler,
-    ]
-    spatial_grid_bounds: jnp.array([-DEFAULT_R_CUT, DEFAULT_R_CUT])
-    num_gridpoints: int = DEFAULT_NUM_GRIDPOINTS
-    n0: float = DEFAULT_N0
-    kT: float = DEFAULT_KT
-    model_params: GaussianBasisPINN = GaussianBasisPINNParams()
-    key: jax.Array = DEFAULT_NN_KEY
+# @dataclass(frozen=True)
+# class SSKKFE: # spherically symmetric KKFE
+#     hnc_dcf: HNCRadialDCF
+#     Uext_handler: typing.Union[
+#         DynamicPotentialHandler,
+#         DynamicNeuralPotentialHandler,
+#     ]
+#     spatial_grid_bounds: jnp.array([-DEFAULT_R_CUT, DEFAULT_R_CUT])
+#     num_gridpoints: int = DEFAULT_NUM_GRIDPOINTS
+#     n0: float = DEFAULT_N0
+#     kT: float = DEFAULT_KT
+#     model_params: GaussianBasisPINN = GaussianBasisPINNParams()
+#     key: jax.Array = DEFAULT_NN_KEY
 
-    # dynamic params
-    m: float = DEFAULT_M
-    friction_coeff: float = DEFAULT_FRICTION_COEFF
-    num_v_gridpoints: int = DEFAULT_NUM_GRIDPOINTS
-    num_v_mean_projections: int = 32
-    ts: jax.Array = jnp.linspace(0., DEFAULT_T)
+#     # dynamic params
+#     m: float = DEFAULT_M
+#     friction_coeff: float = DEFAULT_FRICTION_COEFF
+#     num_v_gridpoints: int = DEFAULT_NUM_GRIDPOINTS
+#     num_v_mean_projections: int = 32
+#     ts: jax.Array = jnp.linspace(0., DEFAULT_T)
 
-    # define scaling limits of the velocity grid (takes positive scalars)
-    v_lim_scale: float = DEFAULT_V_LIMIT_SCALE
+#     # define scaling limits of the velocity grid (takes positive scalars)
+#     v_lim_scale: float = DEFAULT_V_LIMIT_SCALE
 
-    # reference arrays
-    R: jax.Array = field(init=False)
-    dcf_kernel: jax.Array = field(init=False)
-    dcfdr_kernel: jax.Array = field(init=False)
-    rs: jax.Array = field(init=False)
-    vs: jax.Array = field(init=False)
-    v_mean_projections: jax.Array = field(init=False)
+#     # reference arrays
+#     R: jax.Array = field(init=False)
+#     dcf_kernel: jax.Array = field(init=False)
+#     dcfdr_kernel: jax.Array = field(init=False)
+#     rs: jax.Array = field(init=False)
+#     vs: jax.Array = field(init=False)
+#     v_mean_projections: jax.Array = field(init=False)
 
-    # nn bits
-    dynamic_r_density: typing.Callable[
-        [float, float, NNParams], float
-    ] = field(init=False)
-    dynamic_v_density: typing.Callable[
-        [float, float, float, jax.Array, jax.Array], 
-        float
-    ] = field(init = False)
-    valgrad_vs_density: typing.Callable[
-        [float, float, NNParams],
-        jax.Array
-    ] = field(init=False)
-    untrained_params: NNParams = field(init=False)
-    params: NNParams = field(init=False)
+#     # nn bits
+#     dynamic_r_density: typing.Callable[
+#         [float, float, NNParams], float
+#     ] = field(init=False)
+#     dynamic_v_density: typing.Callable[
+#         [float, float, float, jax.Array, jax.Array], 
+#         float
+#     ] = field(init = False)
+#     valgrad_vs_density: typing.Callable[
+#         [float, float, NNParams],
+#         jax.Array
+#     ] = field(init=False)
+#     untrained_params: NNParams = field(init=False)
+#     params: NNParams = field(init=False)
 
-        # pde fns
-    pde_t_residual: typing.Callable[
-        [float, NNParams], 
-        jax.Array,
-    ] = field(init=False)
+#         # pde fns
+#     pde_t_residual: typing.Callable[
+#         [float, NNParams], 
+#         jax.Array,
+#     ] = field(init=False)
     
-    pde_t_residual_loss: typing.Callable[
-        [float, NNParams],
-        float,
-    ] = field(init=False)
+#     pde_t_residual_loss: typing.Callable[
+#         [float, NNParams],
+#         float,
+#     ] = field(init=False)
 
-    valgrad_pde_t_residual_loss: typing.Callable[
-        [float, NNParams],
-        [float, NNParams],
-    ]
+#     valgrad_pde_t_residual_loss: typing.Callable[
+#         [float, NNParams],
+#         [float, NNParams],
+#     ]
     
-    iter_valgrad_pde_causality_loss: typing.Callable[
-        [float, float, float, NNParams, NNParams],
-        [float, float, NNParams, float]
-    ]
+#     iter_valgrad_pde_causality_loss: typing.Callable[
+#         [float, float, float, NNParams, NNParams],
+#         [float, float, NNParams, float]
+#     ]
 
-    valgrad_pde_causality_loss: typing.Callable[
-        [jax.Array, float, NNParams, NNParams],
-        [float, NNParams]
-    ]
+#     valgrad_pde_causality_loss: typing.Callable[
+#         [jax.Array, float, NNParams, NNParams],
+#         [float, NNParams]
+#     ]
 
-    def __post_init__(self):
-        pass
+#     def __post_init__(self):
+#         pass
 
 
 
